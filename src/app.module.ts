fa2a3b5d5894ea7f35bfba9a3ca38d1d389a588d
shinobushiva <stagesp1@gmail.com>
2022-04-29 20:27:42 +0900

    before start graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { UsersModule } from './users/users.module'
 import ormconfig from './config/ormconfig'
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(ormconfig)],
+  imports: [TypeOrmModule.forRoot(ormconfig)],
   controllers: [AppController],
   providers: [AppService],
 })
